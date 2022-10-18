6a3a30373af843396b219ed1fb796c5069ce08c1
Keagan <keagantdickinson@gmail.com>
2022-10-18 15:17:39 +0200

    update

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ function accordion() {
 }
 // event
 btns.forEach((el) => el.addEventListener("click", accordion));
+
+
+
+
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
